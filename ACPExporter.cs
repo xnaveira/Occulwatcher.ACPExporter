9fb82c9e8e766f96d4504dc8049f50de492c700c
Xavier Naveira <xnaveira@gmail.com>
2020-11-11 09:51:40 +0000

    Initial commit

---
+++ ACPExporter.cs
@@ -0,0 +1,157 @@
+﻿using OccultWatcher.SDK;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Occulwatcher.ACPExporter
+{
+    public class ACPExporter : IOWAddin
+    {
+        private IOWHost m_HostInfo = null;
+        private IOWResourceProvider m_ResourceProvider = null;
+        public OWAddinAction[] ADDIN_ACTIONS = null;
+
+        internal string GetResourceString(string resourceName, string defaultValue)
+        {
+            if (m_ResourceProvider == null)
+                // Backwards compatibility with older versions of OW
+                return defaultValue;
+            else
+                return m_ResourceProvider.GetResourceString(resourceName, defaultValue);
+        }
+        public void Configure(System.Windows.Forms.IWin32Window owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string GetStarDirection(IOWUserConfig userSettings, double azimuth)
+        {
+            if (userSettings != null &&
+                userSettings.AzimuthsInDegrees)
+                return string.Format("@{0}°", (int)azimuth);
+
+            if (azimuth <= 0 + 22.5 || azimuth > 360 - 22.5)
+                return GetResourceString("direction.N", "N");
+            if (azimuth <= 45 * 1 + 22.5 && azimuth > 45 * 1 - 22.5)
+                return GetResourceString("direction.NE", "NE");
+            if (azimuth <= 45 * 2 + 22.5 && azimuth > 45 * 2 - 22.5)
+                return GetResourceString("direction.E", "E");
+            if (azimuth <= 45 * 3 + 22.5 && azimuth > 45 * 3 - 22.5)
+                return GetResourceString("direction.SE", "SE");
+            if (azimuth <= 45 * 4 + 22.5 && azimuth > 45 * 4 - 22.5)
+                return GetResourceString("direction.S", "S");
+            if (azimuth <= 45 * 5 + 22.5 && azimuth > 45 * 5 - 22.5)
+                return GetResourceString("direction.SW", "SW");
+            if (azimuth <= 45 * 6 + 22.5 && azimuth > 45 * 6 - 22.5)
+                return GetResourceString("direction.W", "W");
+            if (azimuth <= 45 * 7 + 22.5 && azimuth > 45 * 7 - 22.5)
+                return GetResourceString("direction.NW", "NW");
+
+            return string.Empty;
+        }
+
+        public void ExecuteAction(int actionId, IOWAsteroidEvent astEvent, System.Windows.Forms.IWin32Window owner, OWEventArguments eventArgs)
+        {
+            if (actionId == 1)
+            {
+                if (astEvent != null)
+                {
+                    IOWLocalEventData siteData = astEvent as IOWLocalEventData;
+                    IOWUserConfig userSettings = astEvent as IOWUserConfig;
+                    IOWAsteroidEvent2 evt2 = astEvent as IOWAsteroidEvent2;
+
+                    //TODO: Use the configured format string
+
+                    string errorInTime = string.Empty;
+                    if (evt2 != null &&
+                        evt2.ErrorInTime != -1)
+                    {
+                        if (evt2.ErrorInTime > 90)
+                        {
+                            double err = evt2.ErrorInTime / 60;
+                            errorInTime = string.Format(" +/- {0} min", err.ToString("#0.0"));
+                        }
+                        else
+                            errorInTime = string.Format(" +/- {0} sec", evt2.ErrorInTime.ToString("#0"));
+                    }
+
+                    string maxDuration = string.Empty;
+                    if (astEvent.MaxDuration > 90)
+                    {
+                        double dur = astEvent.MaxDuration / 60;
+                        maxDuration = string.Format("{0} min", dur.ToString("#0.0"));
+                    }
+                    else
+                        maxDuration = string.Format("{0} sec", astEvent.MaxDuration.ToString("#0"));
+
+                    //string eventInfo =
+                    //    astEvent.AsteroidName + " occults " + astEvent.StarName + "; m = " + astEvent.StarMagnitude.ToString() + "; " + ((int)Math.Round(siteData.StarAltitude)).ToString() + "° " + GetStarDirection(userSettings, siteData.StarAzimuth) + "\r\n" +
+                    //    siteData.EventTime.ToString("dd MMM; HH:mm:ss UT") + errorInTime + "; dur: " + maxDuration + "; drop: " + astEvent.MagnitudeDrop.ToString("#0.0") + "m;";
+                    string eventInfo =
+                        "#WaitUntil 1," + siteData.EventTime.ToString("dd/MM/yyyy HH:mm:ss") + " " + maxDuration + "\r\n" + astEvent.AsteroidName + "\t" + siteData.StarAltitude + "\t" + siteData.StarAzimuth;
+
+                    Clipboard.SetText(eventInfo);
+
+                    MessageBox.Show(
+                        owner,
+                        GetResourceString("OWAE.doneText", "The following info has been copied to the ACP file") + "\r\n\r\n" + eventInfo,
+                        GetResourceString("OWAE.AddinName", "ACP Exporter"),
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                }
+            }
+
+            if (actionId == 7)
+            {
+                if (astEvent != null)
+                {
+                    IOWLocalEventData siteData = astEvent as IOWLocalEventData;
+                    IOWUserConfig userSettings = astEvent as IOWUserConfig;
+                    IOWAsteroidEvent2 evt2 = astEvent as IOWAsteroidEvent2;
+                    IOWStationInfo stationInfo = astEvent as IOWStationInfo;
+
+                    //TODO: Observation Report will be prepared here
+                }
+            }
+        }
+
+        public void FinalizeAddin()
+        {
+            // Nothing
+        }
+
+        public OWAddinInfo GetAddinInfo()
+        {
+            return new OWAddinInfo(GetResourceString("OWAE.AddinName", "ACP Exporter"), true, Properties.Resources.ACPExporterIcon.ToBitmap());
+        }
+
+        public void InitializeAddin(IOWHost hostInfo)
+        {
+            m_HostInfo = hostInfo;
+            m_ResourceProvider = hostInfo as IOWResourceProvider;
+
+            ADDIN_ACTIONS = new OWAddinAction[]
+            {
+                new OWAddinAction(1, GetResourceString("OWAE.Action", "Export event to ACP"), OWAddinActionType.SelectedEventAction, Properties.Resources.ACPExporterIcon.ToBitmap()),
+                new OWAddinAction(7, string.Empty, OWAddinActionType.ReportProcessor, null, Color.White)
+            };
+
+        }
+
+        public bool ShouldDisplayAction(int actionId, IOWAsteroidEvent astEvent)
+        {
+            // Our actions will be always displayed for all events
+            return true;
+        }
+
+        public IEnumerable<OWAddinAction> SupportedActions()
+        {
+            return ADDIN_ACTIONS;
+        }
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Occulwatcher.ACPExporter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Occulwatcher.ACPExporter")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("679fd085-1951-4522-8d23-fd873f67c88d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
